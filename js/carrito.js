d66cde038b6b7b669b51f1b64af0ddecd30a12b3
sebasus <susperregui.s@hotmail.com>
2022-08-24 14:29:34 -0300

    segundaEntrega-js

--- js/carrito.js
+++ js/carrito.js
@@ -2,4 +2,22 @@ let recuperoStorage = sessionStorage.getItem('cart')
 
 let storage = JSON.parse(recuperoStorage)
 
-console.log(storage)
+let listaProductos = document.querySelector(".dom")
+
+for(datos of storage){
+    const {agregadoAlCarrito, title, price} = datos
+    let total = agregadoAlCarrito * price
+    listaProductos.innerHTML += `<article class="items">
+                                    <div class="imagen"></div>
+                                    <div class="cantidad">${agregadoAlCarrito}</div>
+                                    <div class="nombre">${title}</div>
+                                    <div class="valor">${price}</div>
+                                    <div class="total">${total}</div>
+                                </article>`
+
+}
+
+
+
+
+
